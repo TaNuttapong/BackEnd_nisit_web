98b7971ff8c7e9260eedde86f7a7a03348e3ca47
TaNuttapong <127654907+TaNuttapong@users.noreply.github.com>
2023-10-09 22:56:39 +0700

    add role super_admin

--- src/models/Request/AccountRequestModel.ts
+++ src/models/Request/AccountRequestModel.ts
@@ -3,14 +3,14 @@ import { RoleEnum } from "../../enums/role.enum";
 export interface AccountRequest {
   email: string;
   password: string;
-  role?: RoleEnum;
+  role: RoleEnum;
   name: string;
   branch: string;
 }
 
 export interface AccountUpdateRequest {
   email: string;
-  role: RoleEnum;
+  role?: RoleEnum;
   name: string;
   branch: string;
 }
